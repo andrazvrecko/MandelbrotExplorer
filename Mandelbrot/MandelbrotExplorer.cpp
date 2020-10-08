3301c20588eeda5f420b45bf87263c2c1cc4cd2f
Andraž Vrečko <andraz.vrecko@gmail.com>
2020-10-08 15:50:29 +0200

    Debug, zoom/drag

--- Mandelbrot/MandelbrotExplorer.cpp
+++ Mandelbrot/MandelbrotExplorer.cpp
@@ -2,6 +2,7 @@
 
 bool MandelbrotExplorer::OnUserCreate()
 {
+	m_pFractal = new int[ScreenWidth() * ScreenHeight()]{ 0 };
 	return true;
 }
 
@@ -12,12 +13,65 @@ bool MandelbrotExplorer::OnUserDestroy()
 
 bool MandelbrotExplorer::OnUserUpdate(float fElapsedTime)
 {
+	olc::vd2d vMouse = { (double)GetMouseX(), (double)GetMouseY() };
+
+	if (GetMouse(0).bPressed)
+	{
+		vStartPan = vMouse;
+	}
+
+	if (GetMouse(0).bHeld)
+	{
+		vOffset -= (vMouse - vStartPan) / vScale;
+		vStartPan = vMouse;
+	}
+
+	olc::vd2d vMouseBeforeZoom;
+	ScreenToWorld(vMouse, vMouseBeforeZoom);
+
+	if (GetKey(olc::Key::Q).bHeld || GetMouseWheel() > 0) vScale *= 1.1;
+	if (GetKey(olc::Key::A).bHeld || GetMouseWheel() < 0) vScale *= 0.9;
+	if (GetKey(olc::Key::K).bPressed) m_bDebug = !m_bDebug;
+
+	olc::vd2d vMouseAfterZoom;
+	ScreenToWorld(vMouse, vMouseAfterZoom);
+	vOffset += (vMouseBeforeZoom - vMouseAfterZoom);
+
+	olc::vi2d c_pixTopLeft = { 0,0 };
+	olc::vi2d c_pixBotRight = { ScreenWidth(), ScreenHeight() };
+	olc::vd2d c_fracTopLeft = { 0.0, 0.0 };
+	olc::vd2d c_fracBotRight = { 0.0, 0.0 };
+
+	ScreenToWorld(c_pixTopLeft, c_fracTopLeft);
+	ScreenToWorld(c_pixBotRight, c_fracBotRight);
+
+	//Start timer
+	auto tp1 = std::chrono::high_resolution_clock::now();
+	
+	simpleFractal(c_pixTopLeft, c_pixBotRight, c_fracTopLeft, c_fracBotRight, m_Iterations);
 	for (int y = 0; y < ScreenHeight(); y++)
 	{
 		for (int x = 0; x < ScreenWidth(); x++)
 		{
-			Draw(x, y, olc::Pixel(122, 122, 122));
+			Draw(x, y, olc::PixelF(100,100,100));
 		}
 	}
+
+	//Stop timer
+	auto tp2 = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> elapsedTime = tp2 - tp1;
+
+	if (m_bDebug) {
+		DrawString(0, 30, "Time Taken: " + std::to_string(elapsedTime.count()) + "s", olc::WHITE, 3);
+		DrawString(0, 80, "Top Left Fractal: " + std::to_string(c_fracTopLeft.x) + " - " + std::to_string(c_fracTopLeft.y));
+		DrawString(0, 90, "Bot Right Fractal: " + std::to_string(c_fracBotRight.x) + " - " + std::to_string(c_fracBotRight.y));
+		DrawString(0, 100, "Top Left Fractal: " + std::to_string(c_pixTopLeft.x) + " - " + std::to_string(c_pixTopLeft.y));
+		DrawString(0, 110, "Bot Right Fractal: " + std::to_string(c_pixBotRight.x) + " - " + std::to_string(c_pixBotRight.y));
+	}
 	return true;
+}
+
+void MandelbrotExplorer::simpleFractal(const olc::vi2d& t_pixTopLeft, const olc::vi2d& t_pixBotRight, const olc::vd2d& t_fracTopLeft, const olc::vd2d& t_fracBotRight, const int& iterations)
+{
+
 }
--- Mandelbrot/MandelbrotExplorer.h
+++ Mandelbrot/MandelbrotExplorer.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Screen.h"
+#include <iostream>
+
 
 class MandelbrotExplorer : public olc::PixelGameEngine
 {
@@ -11,6 +13,29 @@ public:
 	bool OnUserDestroy() override;
 	bool OnUserUpdate(float fElapsedTime) override;
 
+	void simpleFractal(const olc::vi2d &t_pixTopLeft, const olc::vi2d &t_pixBotRight, const olc::vd2d &t_fracTopLeft, const olc::vd2d &t_fracBotRight, const int &iterations);
+
+
+
+	void WorldToScreen(const olc::vd2d& v, olc::vi2d& n)
+	{
+		n.x = (int)((v.x - vOffset.x) * vScale.x);
+		n.y = (int)((v.y - vOffset.y) * vScale.y);
+	}
+
+	void ScreenToWorld(const olc::vi2d& n, olc::vd2d& v)
+	{
+		v.x = (double)(n.x) / vScale.x + vOffset.x;
+		v.y = (double)(n.y) / vScale.y + vOffset.y;
+	}
+
 private:
+	olc::vd2d vOffset = { 0.0, 0.0 };
+	olc::vd2d vStartPan = { 0.0, 0.0 };
+	olc::vd2d vScale = { 1280.0 / 2.0, 720.0 };
+	int* m_pFractal = nullptr;
+	int m_Mode = 0;
+	int m_Iterations = 64;
+	bool m_bDebug = false;
 };
 
